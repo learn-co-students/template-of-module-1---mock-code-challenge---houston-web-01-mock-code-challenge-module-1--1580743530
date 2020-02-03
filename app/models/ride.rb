8e2f25ebbe8c7a8867d898308920a80e131015dc
GianMoran <gpmoran@uh.edu>
2020-02-03 10:35:35 -0600

    Submit

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,21 @@
+class Ride 
+attr_accessor :driver, :passenger, :distance
+@@all = []
+def initialize(driver,passenger,distance)
+ @driver = driver
+ @passenger = passenger 
+ @distance = distance 
+ @@all << self
+end
+def self.all
+    @@all
+end
+def average_distance
+total_distance = 0.0
+avg = 0.0
+rides = @@all.each  {|ride| total_distance += ride.distance}
+avg = total_distance.to_f/rides.length.to_f
 
+end
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -1,10 +1,17 @@
 require_relative '../config/environment.rb'
 
-def reload
-  load 'config/environment.rb'
-end
-
-
+require 'pry'
+require_relative '../config/environment.rb'
+# require_relative 'passenger.rb'
+# require_relative 'ride.rb'
+# def reload
+#   load 'config/environment.rb'
+# end
 # Put your variables here~!
-
+driver1 = Driver.new("Chase")
+passenger1 = Passenger.new("Jase")
+ride1 = Ride.new(driver1,passenger1,100)
+driver2 = Driver.new("Dave")
+passenger2 = Passenger.new("Jake")
+ride2 = Ride.new(driver2,passenger2,500)
 binding.pry
