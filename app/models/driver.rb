8fc3a2dce4e0c4af944f047568168a235c6dd138
placidowang <placidow@gmail.com>
2020-02-03 10:14:21 -0600

    finished

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,1 +1,31 @@
+class Driver
+    attr_accessor 
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def rides
+        Ride.all.select {|ride| ride.driver == self}
+    end
+
+    def passenger_names
+        rides.map {|ride| ride.passenger.name}.uniq
+    end
+
+    def total_distance
+        rides.reduce(0) {|sum, ride| sum + ride.distance}
+    end
+
+    def self.mileage_cap(distance)
+        all.select {|driver| driver.total_distance > distance}
+    end
+
+end
