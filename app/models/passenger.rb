8f148a6e5ad6486ca75af747a12dfa6561ab3e67
corn-cloud <comowaiye@gmail.com>
2020-02-03 10:56:43 -0600

    mock

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,1 +1,42 @@
+class Passenger 
+attr_accessor :name
+@@all = []
 
+def initialize(name)
+    @name = name
+    @@all << self
+end 
+
+def self.all
+    @@all
+end
+
+def rides
+    Ride.all.select {|p| p.passenger == self}
+end
+
+def drivers
+    rides.select {|p| p.passenger == self}
+end
+
+
+def total_distance 
+    rides.find {|p| sum += passenger}
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,25 @@
+class Ride
+attr_accessor :driver, :passenger, :distance 
+@@all = []
 
+def initialize(driver, passenger, distance)
+    @driver = driver
+    @passenger = passenger 
+    @distance = distance 
+    @@all << self
+end
+
+def self.all
+    @@all
+end
+
+def self.average_distance
+    
+end
+
+
+
+
+
+
+end
