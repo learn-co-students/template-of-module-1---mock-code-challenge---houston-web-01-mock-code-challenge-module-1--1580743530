d5786c16669d656eb620b851595fc1e07973c36d
DrVonDevious <drvondevious@gmail.com>
2020-02-03 10:44:44 -0600

    First Commit

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,1 +1,35 @@
 
+class Driver
+
+  attr_accessor :name, :rides, :passengers
+
+  @@all = []
+
+  def initialize(name)
+    self.name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def rides
+    Ride.all.select { |r| r.driver == self }
+  end
+
+  def passengers
+    rides.map { |r| r.passenger }
+  end
+
+  def total_miles_driven
+    rides.sum { |r| r.distance }
+  end  
+
+  def self.mileage_cap(distance)
+    @@all.select { |d| d.total_miles_driven > distance }
+  end  
+
+end  
+
+
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,1 +1,23 @@
 
+class Ride
+
+  attr_accessor :distance, :passenger, :driver
+
+  @@all = []
+
+  def initialize(distance, passenger, driver)
+    self.distance = distance
+    self.passenger = passenger 
+    self.driver = driver
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.average_distance
+    @@all.distance.inject(0.0) { |sum, el| sum + el } / @@all.distance.size
+  end  
+
+end  
